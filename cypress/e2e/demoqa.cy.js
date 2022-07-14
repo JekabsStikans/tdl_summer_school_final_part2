5338e272e6037f7990ec47f1513b73fffe866bc9
JekabsStikans <stikans.jekabs@gmail.com>
2022-07-14 11:55:01 +0300

    Scenario 1 - practice form

--- cypress/e2e/demoqa.cy.js
+++ cypress/e2e/demoqa.cy.js
@@ -4,32 +4,58 @@ import PracticeFormPage from "../pageObjects/PracticeFormPage";
 
 describe("Demoqa testing", () => {
 
-    it.only("1. - Forms - Practice forms", () => {
+    it("1. - Forms - Practice forms", () => {
         PracticeFormPage.visit();
         // - Fill in information
         //     - First Name
+        PracticeFormPage.firstNameInput.type("Name");
         //     - Last Name
+        PracticeFormPage.lastNameInput.type("Surname");
         //     - Email
+        PracticeFormPage.userEmailInput.type("fake@mail.com");
         //     - Gender
+        PracticeFormPage.genderWrapper.contains("Male").click();
         //     - Mobile number
+        PracticeFormPage.userNumberInput.type("5647382976");
         //     - Date of birth
+        PracticeFormPage.dateOfBirthInput.click();
         //         - Set value - 29/February/1984
         //         - Set date of birth value by using the calendar widget
         //         - Pass the values dynamically, e.g. create methods:
         //             - setYear(“1984”)
+        PracticeFormPage.setYear("1984");
         //             - setMonth(“February”)
+        PracticeFormPage.setMonth("February");
         //             - “setDate(“29”)”
+        PracticeFormPage.setDate(29);
         //     - Subjects
         //         - Maths
+        PracticeFormPage.subjectsInput.type("Math{enter}");
         //     - Hobbies
+        PracticeFormPage.hobbiesWrapper.contains("Music").click();
         //     - Current address
+        PracticeFormPage.currentAddressInput.type("Random street 57");
         //     - State
+        PracticeFormPage.stateSelector.click();
+        PracticeFormPage.stateSelector.type("Haryana{enter}");
         //     - City
+        PracticeFormPage.citySelector.click();
+        PracticeFormPage.citySelector.type("Karnal{enter}");
         // - Click Submit
+        PracticeFormPage.submitButton.click();
         // - Validate Values in the table based on label value
+        PracticeFormPage.finalTable.contains("Student Name").parent().contains("Name Surname").should("exist");
+        PracticeFormPage.finalTable.contains("Student Email").parent().contains("fake@mail.com").should("exist");
+        PracticeFormPage.finalTable.contains("Gender").parent().contains("Male").should("exist");
+        PracticeFormPage.finalTable.contains("Mobile").parent().contains("5647382976").should("exist");
+        PracticeFormPage.finalTable.contains("Date of Birth").parent().contains("29 February,1984").should("exist");
+        PracticeFormPage.finalTable.contains("Subjects").parent().contains("Maths").should("exist");
+        PracticeFormPage.finalTable.contains("Hobbies").parent().contains("Music").should("exist");
+        PracticeFormPage.finalTable.contains("Address").parent().contains("Random street 57").should("exist");
+        PracticeFormPage.finalTable.contains("State and City").parent().contains("Haryana Karnal").should("exist");	
     });
 
-    it("2. - Interactions - Sortable", () => {
+    it.only("2. - Interactions - Sortable", () => {
         //   - ( https://demoqa.com/sortable )  - 
         //     - Validate that the values are in order - One, Two, Three, Four, Five, Six
         //     - Sort the values in following order - Six, Five, Four, Three, Two, One
--- cypress/pageObjects/PracticeFormPage.js
+++ cypress/pageObjects/PracticeFormPage.js
@@ -5,8 +5,76 @@ class PracticeFormPage extends BasePage {
     return "https://demoqa.com/automation-practice-form";
   }
 
-  static get usernameInput(){
-    return cy.get('[data-test="username"]');
+  static get firstNameInput(){
+    return cy.get('#firstName');
+  }
+
+  static get lastNameInput(){
+    return cy.get('#lastName');
+  }
+
+  static get userEmailInput(){
+    return cy.get('#userEmail');
+  }
+
+  static get genderWrapper(){
+    return cy.get('#genterWrapper');
+  }
+
+  static get userNumberInput(){
+    return cy.get('#userNumber');
+  }
+
+  static get dateOfBirthInput(){
+    return cy.get('#dateOfBirthInput');
+  }
+
+  static get dateOfBirthPicker(){
+    return cy.get('.react-datepicker');
+  }
+
+  static setYear(text){
+    this.dateOfBirthPicker.find(".react-datepicker__year-select").select(text);
+  }
+
+  static setMonth(text){
+    this.dateOfBirthPicker.find(".react-datepicker__month-select").select(text);
+  }
+
+  static setDate(number){
+    if(number > 15){
+      this.dateOfBirthPicker.find("[aria-label*= "+ number.toString() +"]").last().click();
+    }else{
+      this.dateOfBirthPicker.find("[aria-label*= "+ number.toString() +"]").first().click();
+    }
+  }
+
+  static get subjectsInput(){
+    return cy.get('#subjectsInput');
+  }
+
+  static get hobbiesWrapper(){
+    return cy.get('#hobbiesWrapper');
+  }
+
+  static get currentAddressInput(){
+    return cy.get('#currentAddress');
+  }
+
+  static get stateSelector(){
+    return cy.get('#state');
+  }
+
+  static get citySelector(){
+    return cy.get('#city');
+  }
+
+  static get submitButton(){
+    return cy.get('#submit');
+  }
+
+  static get finalTable(){
+    return cy.get('tbody');
   }
 }
 
